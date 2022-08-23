a81aa77e7d98022839fdbb29dd1d80439a95e583
Gaurav Singla <gauravsingla1012@gmail.com>
2022-08-23 15:50:39 +0530

    index.js modified

--- src/index.js
+++ src/index.js
@@ -2,6 +2,5 @@ import react from 'react';
 import reactdom from 'react-dom';
 import Todolist from './Todolist.js';
 import './index.css'
-// import App from './App.js';
 
 reactdom.render(<Todolist />, document.getElementById('root'));
